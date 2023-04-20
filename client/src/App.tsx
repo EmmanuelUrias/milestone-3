47132fe525bc773fc16851a41c8c5a006beb6a17
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-19 20:35:11 -0700

    Making planning progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,15 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+// On mobile just make it all vertical and navbar horizontal
   return (
     <div className="App">
+      {/* Navbar */} {/* I want navbar to be vertical */}
       <div className="card">
+        {/* ExpenseFeed */} {/* Center of the page */}
+        {/* MonthlyExpenseAmount */} {/* Next to the expense feed on its right on non-mobile devices */}
+        {/* Goal */} {/* On feeds right side but under MonthlyExpenseAmount */}
+        {/* AddGoal */} {/* under the feed */}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
---
+++ client/src/components/IndividualExpense.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IndividualExpense = () => {
+    //fetch for each individual Id coming in from the props, SideNote: Might use some sort of state management for this data
+
+    //populate data from props
+  return (
+    <div>IndividualExpense</div>
+  )
+}
+
+export default IndividualExpense
