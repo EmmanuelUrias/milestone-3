1573b2772ebaa316f9d0c6c32951deffe8f0337f
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-20 08:26:01 -0700

    finally got the server to run, want to make models and auth routes next

--- server/budget_buddy_api/__init__.py
+++ server/budget_buddy_api/__init__.py
@@ -1,11 +1,26 @@
+from dotenv import dotenv_values
 from flask import Flask
 
 
 def create_app():
     app = Flask(__name__)
 
+    URI = dotenv_values('.env')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'{URI}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     @app.route('/')
     def hello():
         return 'Hello this is your Budget Buddy!!'
+
+    # from .models import Users
+    # Users.db.init_app(app)
+
+    from . import users_controller
+    app.register_blueprint(users_controller.bp)
+
+    if __name__ == '__main__':
+        app.run()
 
     return app
