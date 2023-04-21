d1f38be4a96c545c68e5f280abd451f1d933ed0b
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-20 21:08:25 -0700

    good progress with the server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import helmet from 'helmet'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
+import { Sequelize } from 'sequelize'
+
 // Configurations
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -14,4 +16,13 @@ dotenv.config()
 
 const app = express()
 app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' })) // allows cross origin reqs but can't access res content
 app.use(morgan('common'))
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.listen(process.env.PORT, () => {
+    console.log(`Your running on ${process.env.PORT} 😼`)
+})
