cbfda27ef32bf51e13443e49e05b4e6f5ae17911
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-26 19:54:10 -0700

    made register form, going to test to make sure it works

--- client/src/scenes/LoginPage.tsx
+++ client/src/scenes/LoginPage.tsx
@@ -1,9 +1,49 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, FormLabel, Input, FormControl, InputLabel } from '@mui/material'
 import React, { useState } from 'react'
 
 const LoginPage = () => {
   const [isToRegister, setIsToRegister] = useState(true)
+  const [message, setMessage] = useState('')
+  const [values, setValues] = useState({
+    user_name: '',
+    password: '',
+    email: '',
+    budget: 0
+  })
+  
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {id, value} = event.target
+    setValues({...values, [id]: value})
+  }
 
+  const register = async (event: any) => {
+    event.preventDefault()
+    const user = {
+      user_name: values.user_name,
+      password: values.password,
+      email: values.email,
+      budget: values.budget,
+      time_stamp: new Date
+    }
+
+    const newUserRes = await fetch('', {
+      method: 'POST',
+      headers: {
+        'Content_Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+
+    const newUser = await newUserRes.json()
+
+    if(newUser) {
+      setMessage('User already exists')
+    }
+  }
+
+  const logIn = async (event: any) => {
+
+  }
 
   return (
     <Box>
@@ -19,12 +59,18 @@ const LoginPage = () => {
         }
         }}>
           <Typography variant='h3'>Register</Typography>
-          <form>
-            <label>Hello</label>
-            <input />
-            <input />
-            <input />
-            <input />
+          <form onSubmit={register}>
+            <FormControl>
+              <FormLabel>Username</FormLabel>
+              <Input id='user_name' onChange={handleChange} value={values.user_name} type='string' required/>
+              <FormLabel>Password</FormLabel>
+              <Input id='password' onChange={handleChange} value={values.password} type='string' required/>
+              <FormLabel>Email</FormLabel>
+              <Input id='email' onChange={handleChange} value={values.email} type='string' required/>
+              <FormLabel>Budget</FormLabel>
+              <Input id='budget' onChange={handleChange} value={values.budget} type='number' required/>
+            </FormControl>
+            <button type='submit'>Register</button>
           </form>
             <a onClick={() => setIsToRegister(false)}>Already have an account</a>
         </Box>
@@ -39,11 +85,13 @@ const LoginPage = () => {
             flexDirection: 'column'
         }
       }}>
+        <form onSubmit={logIn}>
         <Typography variant='h3'>Log In</Typography>
-          <form>
-            <input />
-            <input />
-          </form>
+          <FormControl>
+            <Input />
+            <Input />
+          </FormControl>
+        </form>
           <a onClick={() => setIsToRegister(true)}>Don't have an account</a>
         </Box>
       ) }
